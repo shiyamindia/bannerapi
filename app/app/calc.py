4901d066c4c50fbfebe248289a69473e7fb04e8e
shiyamak <shiyam.sundar@ak.com.sa>
2022-09-06 16:06:06 +0530

    updated test cases

---
+++ app/app/calc.py
@@ -0,0 +1,7 @@
+"""
+Calculator functiomn
+"""
+
+def add(x, y):
+    """ add x and y and returns the total """
+    return x + y
---
+++ app/app/tests.py
@@ -0,0 +1,17 @@
+"""
+sample test using calc
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CaldTests(SimpleTestCase):
+    """ test the calc module """
+
+    def test_add_numbers(self):
+        """ test adding numbers """
+
+        result = calc.add(5, 6)
+
+        self.assertEqual(result, 12)
